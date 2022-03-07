11adbf310e702586f43d62505cc26fb1a18d6613
Sayf Ahmed <P2597539@my365.dmu.ac.uk>
2022-03-07 19:26:12 +0000

    hgfgffh

--- ClassLibrary/clsStock.cs
+++ ClassLibrary/clsStock.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStock
     {
+        public bool Available { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string GameDescription { get; set; }
+        public int Price { get; set; }
+        public int AgeRating { get; set; }
+        public int GameNumber { get; set; }
     }
 }
